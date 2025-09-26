6b5f6a60550ed102dbcb04fb2e28d93d3e6f3126
Vivian Gichure <viviangichure@gmail.com>
2025-09-26 21:35:23 +0300

    updated from local host

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
 
     CORS(
         app,
-        resources={r"/*": {"origins": "http://localhost:3000"}},
+        resources={r"/*": {"origins": "http://localhost:3000,https://pos-app-orcin-chi.vercel.app/"}},
         supports_credentials=True,
     )
 
