f55ec6781680f29c4b79dc1113d2e2781242db66
Vivian Gichure <viviangichure@gmail.com>
2025-09-26 22:09:50 +0300

    updated url from local

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://shoe-world-base.onrender.com",
+  baseURL: "https://shoe-world-base.onrender.com", 
   withCredentials: true,
 });
 
